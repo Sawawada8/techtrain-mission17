465ba8c1655cd6408a405011dd90f3ea5d8967e4
sawada <sawada@csplan.jp>
2021-12-16 16:10:32 +0900

    [add]routing 機能追加

--- src/Application.php
+++ src/Application.php
@@ -1,11 +1,36 @@
 <?php
 namespace MyFrameWork;
 
+use Exception;
 use MyFrameWork\Routing\Route;
+use App\Controllers;
 
 class Application
 {
-    public function routing(Route $route, $url)
+    /**
+     * pathに対応するアクションを呼び出す。
+     */
+    public function routing(Route $route, $path, $method)
     {
+        switch ($method) {
+            case "GET":
+                $action = $route->getRoutes[$path];
+                break;
+            case "POST":
+                $action = $route->postRoutes[$path];
+                break;
+            case "PUT":
+                $action = $route->putRoutes[$path];
+                break;
+        }
+
+        if (is_null($action)) {
+            throw new Exception("not defined !!!!");
+        }
+
+        $controller = "App\\Controllers\\" . $action["controller"];
+        $action = $action["action"];
+
+        (new $controller())->$action();
     }
 }
